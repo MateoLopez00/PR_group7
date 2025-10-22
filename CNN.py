6981004a50fe8ab050af83f1001320c5ac8ddb50
CarloRobbiani <79840218+CarloRobbiani@users.noreply.github.com>
2025-10-22 15:06:11 +0200

    Changed python version + comments

--- CNN.py
+++ CNN.py
@@ -1,5 +1,3 @@
-import tensorflow as tf
-
 #from tensorflow import datasets, layers, models
 import matplotlib.pyplot as plt
 from keras import layers, models
@@ -7,8 +5,8 @@ import pandas as pd
 from PIL import Image
 import numpy as np
 
-df_test = pd.read_csv('MNIST-full\gt-test.tsv', sep='\t', header=None, names=['image_path', 'label'])
-df_train = pd.read_csv('MNIST-full\gt-train.tsv', sep='\t', header=None, names=['image_path', 'label'])
+df_test = pd.read_csv('MNIST-full/gt-test.tsv', sep='\t', header=None, names=['image_path', 'label'])
+df_train = pd.read_csv('MNIST-full/gt-train.tsv', sep='\t', header=None, names=['image_path', 'label'])
 
 
 def load_image(image_path):
@@ -25,6 +23,8 @@ labels_train = df_train['label'].values
 # Load images
 test_images = np.array([load_image("MNIST-full/" + path) for path in image_paths_test])
 train_images = np.array([load_image("MNIST-full/" + path) for path in image_paths_train])
+
+# TODO make a val set from the train set
 
 # Define a simple CNN model
 model = models.Sequential([
@@ -46,3 +46,5 @@ model.compile(optimizer='adam',
 # Train the model
 model.fit(train_images, labels_train, validation_data=(test_images, labels_test), epochs=5, batch_size=32)
 # accuracy: 0.9949 - loss: 0.0170 - val_accuracy: 0.9906 - val_loss: 0.0291
+
+# TODO validate on the test set and record findings / loss and accuracy curves for report
