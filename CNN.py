fe64bcfcf4217000d877abc0d7df4707d2d69a9a
CarloRobbiani <79840218+CarloRobbiani@users.noreply.github.com>
2025-10-28 11:19:42 +0100

    added accuracy plot for cnn

--- CNN.py
+++ CNN.py
@@ -4,6 +4,7 @@ from keras import layers, models
 import pandas as pd
 from PIL import Image
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 df_test = pd.read_csv('MNIST-full/gt-test.tsv', sep='\t', header=None, names=['image_path', 'label'])
 df_train = pd.read_csv('MNIST-full/gt-train.tsv', sep='\t', header=None, names=['image_path', 'label'])
@@ -23,6 +24,10 @@ labels_train = df_train['label'].values
 # Load images
 test_images = np.array([load_image("MNIST-full/" + path) for path in image_paths_test])
 train_images = np.array([load_image("MNIST-full/" + path) for path in image_paths_train])
+
+val_images, train_images, val_labels, train_labels = train_test_split(
+    train_images, labels_train, test_size=0.8, random_state=42, stratify=labels_train
+)
 
 # TODO make a val set from the train set
 
@@ -44,7 +49,26 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Train the model
-model.fit(train_images, labels_train, validation_data=(test_images, labels_test), epochs=5, batch_size=32)
-# accuracy: 0.9949 - loss: 0.0170 - val_accuracy: 0.9906 - val_loss: 0.0291
+history = model.fit(train_images, train_labels,
+                    validation_data=(val_images, val_labels),
+                    epochs=5, batch_size=32)
+
+# Save the trained model
+model.save('cnn_model.keras')
+
+test_loss, test_acc = model.evaluate(test_images, labels_test, verbose=0)
 
 # TODO validate on the test set and record findings / loss and accuracy curves for report
+val_acc = history.history.get('val_accuracy', history.history.get('val_acc'))
+epochs = range(1, len(val_acc) + 1)
+
+plt.figure()
+plt.plot(epochs, val_acc, 'b-', label='Validation accuracy')
+plt.hlines(test_acc, epochs[0], epochs[-1], colors='r', linestyles='--', label=f'Test accuracy = {test_acc:.4f}')
+plt.title('Validation accuracy per epoch and final test accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.grid(True)
+plt.savefig('accuracy_plot_cnn_k=3.png')
+plt.show()
