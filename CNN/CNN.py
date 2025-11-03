6b31a7f93c2f6d7d3915776f874db2b66879aad9
CarloRobbiani <79840218+CarloRobbiani@users.noreply.github.com>
2025-11-03 16:22:32 +0100

    Changed folder structure + added Report.ipynb

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -1,35 +1,30 @@
 #from tensorflow import datasets, layers, models
 import matplotlib.pyplot as plt
 from keras import layers, models
-import pandas as pd
-from PIL import Image
-import numpy as np
 from sklearn.model_selection import train_test_split
+import numpy as np
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import utils.data_loader as data_loader
 
-df_test = pd.read_csv('MNIST-full/gt-test.tsv', sep='\t', header=None, names=['image_path', 'label'])
-df_train = pd.read_csv('MNIST-full/gt-train.tsv', sep='\t', header=None, names=['image_path', 'label'])
 
 
-def load_image(image_path):
-    img = Image.open(image_path)
-    img_array = np.array(img)  # Convert image to array
-    img_array = img_array / 255.0  # Normalize to [0, 1]
-    return img_array
-# Load all images and labels
-image_paths_test = df_test['image_path'].values
-labels_test = df_test['label'].values
-image_paths_train = df_train['image_path'].values
-labels_train = df_train['label'].values
 
-# Load images
-test_images = np.array([load_image("MNIST-full/" + path) for path in image_paths_test])
-train_images = np.array([load_image("MNIST-full/" + path) for path in image_paths_train])
+X_train, y_train, X_val, y_val = data_loader.get_train_and_validation_set() 
+X_test, y_test = data_loader.get_test_data()
 
-val_images, train_images, val_labels, train_labels = train_test_split(
-    train_images, labels_train, test_size=0.8, random_state=42, stratify=labels_train
-)
+def ensure_4d(X):
+    X = np.asarray(X)
+    if X.ndim == 2 and X.shape[1] == 28*28:
+        X = X.reshape((-1, 28, 28, 1))
+    elif X.ndim == 3 and X.shape[1] == 28 and X.shape[2] == 28:
+        X = X.reshape((-1, 28, 28, 1))
+    return X.astype('float32')
 
-# TODO make a val set from the train set
+X_train = ensure_4d(X_train)
+X_val   = ensure_4d(X_val)
+X_test  = ensure_4d(X_test)
 
 # Define a simple CNN model
 model = models.Sequential([
@@ -49,14 +44,14 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Train the model
-history = model.fit(train_images, train_labels,
-                    validation_data=(val_images, val_labels),
+history = model.fit(X_train, y_train,
+                    validation_data=(X_val, y_val),
                     epochs=5, batch_size=32)
 
 # Save the trained model
-model.save('cnn_model.keras')
+model.save('CNN/cnn_model.keras')
 
-test_loss, test_acc = model.evaluate(test_images, labels_test, verbose=0)
+test_loss, test_acc = model.evaluate(X_test, y_test, verbose=0)
 
 train_acc = history.history.get('accuracy') or history.history.get('acc')
 val_acc = history.history.get('val_accuracy') or history.history.get('val_acc')
@@ -89,5 +84,5 @@ axs[1].legend()
 axs[1].grid(True)
 
 plt.tight_layout()
-plt.savefig('accuracy_loss_cnn.png')
+plt.savefig('CNN/accuracy_loss_cnn.png')
 plt.show()
