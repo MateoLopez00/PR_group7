bb9b75895d22bee1936b39e8d3076a670d07c1d8
Viola Meier <viola.camille.meier@gmail.com>
2025-11-15 13:21:56 +0100

    fix indices for sakoe_chiba_band

--- KWS/DTW.py
+++ KWS/DTW.py
@@ -15,7 +15,8 @@ def DTW(seq1, seq2, win_size=0.5):
     """
     T1 = seq1.shape[0]
     T2 = seq2.shape[0]
-
+    # window[0, i] = j_start for given i 
+    # window[1, i] = j_end for given i 
     window = sakoe_chiba_band(T1, T2, win_size)
 
     D = np.full((T1 + 1, T2 + 1), np.inf)
@@ -23,8 +24,10 @@ def DTW(seq1, seq2, win_size=0.5):
 
     # Compute the cost with the window
     for i in range(1, T1 + 1):
-        j_min = max(1, i - window)
-        j_max = min(T2, i + window)
+        # get valid range for j 
+        j_min = max(0,window[0,i-1]) + 1     
+        j_max = min(T2, window[1,i-1]) 
+     
         for j in range(j_min, j_max + 1):
             cost = _euclidean(seq1[i - 1], seq2[j - 1])
             D[i, j] = cost + min(D[i - 1, j],       #insertion
